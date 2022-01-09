772ce2b6aa2760a7dcd82c02fd8868e67886c01e
Matt Kolbach <mattkolbach@yahoo.com>
2022-01-09 11:06:54 -0600

    second API call works, 5 day cards are created

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,58 +1,111 @@
 const apiKey = "842c3948b50987469451e4a23adc7e2e";
 const citySearchButton = document.querySelector("#userCityButton");
 const cityNameEl = document.querySelector("#userCityInput");
-let citySearchHistory = [];
+let citySearchHistory = JSON.parse(localStorage.getItem("cityHistory")) || [];
 const cityHistoryColumnEl = document.querySelector("#cityHistory");
 const currentCityweatherEl = document.getElementById("currentCityweather");
 let today = new Date().toLocaleDateString();
+let currentCityLat;
+let currentCityLon;
+const fiveDayEl = document.querySelector("#fiveDay");
 
 ////////////////////////////////////global variables above///////////////////////////////////////////////
 
-//////////     API call start     //////////
-let getCurrentWeatherHandler = function (event) {
+//////////     first API call     //////////
+const getCurrentWeatherHandler = function (event) {
   const cityName = cityNameEl.value.trim();
   //clear old content from input on screen
   cityNameEl.value = "";
-  
-  let currentWeatherApiUrl =
+
+  const currentWeatherApiUrl =
     "http://api.openweathermap.org/data/2.5/weather?q=" +
     cityName +
     "&units=imperial" +
     "&appid=" +
     apiKey;
-  console.log(currentWeatherApiUrl);
+  console.log("current weather api: " + currentWeatherApiUrl);
   fetch(currentWeatherApiUrl).then(function (response) {
     if (response.ok) {
       citySearchHandler(cityName);
       console.log(response);
       response.json().then(function (data) {
+        console.log("current weather data: ");
         console.log(data);
-        const currentCity = document.createElement("h2"); //creating tag for current city
+        const currentCityName = document.createElement("h2"); //creating tag for current city
         const currentCityTemp = document.createElement("p");
         const currentCityHumidity = document.createElement("p");
         const currentCityWind = document.createElement("p");
-        //const currentCityUVIndex = document.createElement("p");
 
-        currentCity.textContent = `${data.name}, ${today}`; //grab the name from the data
+        currentCityName.textContent = `${data.name}, ${today}`; //grab the name from the data
         currentCityTemp.textContent = `Temp: ${data.main.temp} °F`;
         currentCityHumidity.textContent = `Humidity: ${data.main.humidity} %`;
         currentCityWind.textContent = `Wind: ${data.wind.speed} MPH`;
-        //currentCityUVIndex.textContent = `UV Index: `
 
         currentCityweatherEl.append(
-          currentCity,
+          currentCityName,
           currentCityTemp,
           currentCityHumidity,
           currentCityWind
         );
+
+        currentCityLat = data.coord.lat;
+        currentCityLon = data.coord.lon;
+        console.log(currentCityLon);
+        console.log(currentCityLat);
+        oneCallWeatherHandler();
       });
     }
   });
 };
 
+
+/////  second API call  /////
+const oneCallWeatherHandler = function () {
+  const oneCallApiUrl =
+    "https://api.openweathermap.org/data/2.5/onecall?lat=" +
+    currentCityLat +
+    "&lon=" +
+    currentCityLon +
+    "&exclude=minutely,hourly&units=imperial&appid=" +
+    apiKey;
+  console.log("one call api:");
+  console.log(oneCallApiUrl);
+  fetch(oneCallApiUrl).then(function (response) {
+    console.log("one call response: " + response);
+    response.json().then(function (data) {
+      console.log("one call data:");
+      console.log(data);
+      const currentCityUvi = document.createElement("p");
+
+      currentCityUvi.textContent = `UVI: ${data.current.uvi}`;
+      currentCityweatherEl.append(currentCityUvi);
+      fiveDayElHandler(data.daily);
+    });
+  });
+};
+
+const fiveDayElHandler = function(fiveDayData) {
+  fiveDayEl.innerHTML = ""
+  for (let i=1; i<6; i++){
+    let newDate = new Date(fiveDayData[i].dt*1000)
+    let newDiv = document.createElement("div");
+    newDiv.classList.add("col-lg-2")
+    let newEl = `<div class="card" style="width: 18rem;">
+        <img src="http://openweathermap.org/img/wn/${fiveDayData[i].weather[0].icon}@2x.png" class="card-img-top" alt="...">
+        <div class="card-body">
+          <h5 class="card-title">${newDate}</h5>
+          <p class="card-text">Temp: ${fiveDayData[i].temp.day} </p>
+          <p class="card-text">Wind: ${fiveDayData[i].wind_speed}</p>
+          <p class="card-text">Humidity: ${fiveDayData[i].humidity}</p>
+        </div>
+      </div>`
+newDiv.innerHTML = newEl;
+fiveDayEl.append(newDiv);
+  }
+};
+
 ////////this captures the city name when the search button is clicked////////
 var citySearchHandler = function (cityName) {
-
   //pushing cityName to citySearchHistory array
   if (!citySearchHistory.includes(cityName)) {
     citySearchHistory.push(cityName); //pushes to citySearchHistory array
@@ -60,11 +113,13 @@ var citySearchHandler = function (cityName) {
     cityButtonFactory(cityName);
   }
 
-  saveCity(citySearchHistory);//pass citySerchHistory to the saveCity function
+  saveCity(citySearchHistory); //pass citySearchHistory to the saveCity function
 };
 
+
 /////     saving city name to local storage    /////
-let saveCity = function (cityHistory) {//cityHistory was citySearchHistory
+let saveCity = function (cityHistory) {
+  //cityHistory was citySearchHistory
   localStorage.setItem("cityHistory", JSON.stringify(cityHistory));
 };
 
@@ -85,24 +140,14 @@ const cityButtonFactory = function (cityName) {
   cityHistoryColumnEl.appendChild(citySearchHistoryEl);
 };
 
-
 /////     Load search history on page load     /////
 const loadSearchHistory = function () {
   const loadCityHistory = localStorage.getItem("cityHistory");
   const parsedCityHistory = JSON.parse(loadCityHistory);
   console.log(parsedCityHistory);
   parsedCityHistory.forEach(cityButtonFactory);
+  parsedCityHistory.forEach(saveCity);
 };
-
-/*
-let testQueryURL =
-  "http://api.openweathermap.org/data/2.5/weather?q=" +
-  "milwaukee" +
-  "&units=imperial" +
-  "&appid=" +
-  apiKey;
-console.log(testQueryURL);
-*/
 
 //////////////////////////////////////////////////////////////////////////////////////////
 // add event listeners
@@ -128,3 +173,18 @@ THEN I am again presented with current and future conditions for that city*/
 //get data
 //pick stuff out of array that I need
 //plug data into cards
+
+//capture lat and longitude from first api
+//feed lat and lon into second API call
+//capture UV index from second API
+//put UV index on page
+//capture 5 of 7 days forcast from second API call
+//put forcast on page
+
+//capture clicking on historical city buttons to search
+
+//fix bugs
+
+//change input to form?
+
+//finalize css/bootstrap
