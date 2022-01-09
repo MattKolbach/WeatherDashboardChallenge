25b1efe00b4322f638ab1a6534ccb2f1c70a4fa9
Matt Kolbach <mattkolbach@yahoo.com>
2022-01-08 21:33:04 -0600

    cities now go to array, buttons are created on load

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,31 +1,80 @@
 const apiKey = "842c3948b50987469451e4a23adc7e2e";
-let cityName;
 const citySearchButton = document.querySelector("#userCityButton");
 const cityNameEl = document.querySelector("#userCityInput");
 let citySearchHistory = [];
 const cityHistoryColumnEl = document.querySelector("#cityHistory");
 const currentCityweatherEl = document.getElementById("currentCityweather");
+let today = new Date().toLocaleDateString();
+
 ////////////////////////////////////global variables above///////////////////////////////////////////////
 
-////////this captures the city name when the search button is clicked////////
-var citySearchHandler = function (event) {
-  // prevent page from refreshing
-  event.preventDefault();
-
-  // get value from input element
-  cityName = cityNameEl.value.trim();
-
+//////////     API call start     //////////
+let getCurrentWeatherHandler = function (event) {
+  const cityName = cityNameEl.value.trim();
   //clear old content from input on screen
   cityNameEl.value = "";
+  
+  let currentWeatherApiUrl =
+    "http://api.openweathermap.org/data/2.5/weather?q=" +
+    cityName +
+    "&units=imperial" +
+    "&appid=" +
+    apiKey;
+  console.log(currentWeatherApiUrl);
+  fetch(currentWeatherApiUrl).then(function (response) {
+    if (response.ok) {
+      citySearchHandler(cityName);
+      console.log(response);
+      response.json().then(function (data) {
+        console.log(data);
+        const currentCity = document.createElement("h2"); //creating tag for current city
+        const currentCityTemp = document.createElement("p");
+        const currentCityHumidity = document.createElement("p");
+        const currentCityWind = document.createElement("p");
+        //const currentCityUVIndex = document.createElement("p");
+
+        currentCity.textContent = `${data.name}, ${today}`; //grab the name from the data
+        currentCityTemp.textContent = `Temp: ${data.main.temp} °F`;
+        currentCityHumidity.textContent = `Humidity: ${data.main.humidity} %`;
+        currentCityWind.textContent = `Wind: ${data.wind.speed} MPH`;
+        //currentCityUVIndex.textContent = `UV Index: `
+
+        currentCityweatherEl.append(
+          currentCity,
+          currentCityTemp,
+          currentCityHumidity,
+          currentCityWind
+        );
+      });
+    }
+  });
+};
+
+////////this captures the city name when the search button is clicked////////
+var citySearchHandler = function (cityName) {
 
   //pushing cityName to citySearchHistory array
-  citySearchHistory.push(cityName);
-  console.log(citySearchHistory);
+  if (!citySearchHistory.includes(cityName)) {
+    citySearchHistory.push(cityName); //pushes to citySearchHistory array
+    console.log(citySearchHistory);
+    cityButtonFactory(cityName);
+  }
 
-  saveCity();
-  loadCity();
+  saveCity(citySearchHistory);//pass citySerchHistory to the saveCity function
+};
 
-  ///create button for historical city///
+/////     saving city name to local storage    /////
+let saveCity = function (cityHistory) {//cityHistory was citySearchHistory
+  localStorage.setItem("cityHistory", JSON.stringify(cityHistory));
+};
+
+/////    load saved cities name to page(history button)    /////
+let loadCity = function () {
+  let loadedCityName = localStorage.getItem("cityHistory");
+};
+
+/////     create button for historical city    /////
+const cityButtonFactory = function (cityName) {
   let citySearchHistoryEl = document.createElement("button");
   //add type
   citySearchHistoryEl.type = "button";
@@ -36,47 +85,13 @@ var citySearchHandler = function (event) {
   cityHistoryColumnEl.appendChild(citySearchHistoryEl);
 };
 
-/////     saving city name to local storage    /////
-let saveCity = function () {
-  localStorage.setItem("cityHistory", JSON.stringify(cityName));
-};
 
-/////    load saved cities name to page(history button)    /////
-let loadCity = function () {
-  let loadedCityName = localStorage.getItem("cityHistory");
-};
-
-//////////     API call start     //////////
-let getCurrentWeatherHandler = function (event) {
-  let currentWeatherApiUrl =
-    "http://api.openweathermap.org/data/2.5/weather?q=" +
-    cityName +
-    "&units=imperial" +
-    "&appid=" +
-    apiKey;
-  console.log(currentWeatherApiUrl);
-  fetch(currentWeatherApiUrl)
-  .then(function (response) {
-    if (response.ok) {
-      console.log(response);
-      response.json().then(function (data) {
-        console.log(data);
-        const currentCity = document.createElement("h2");//creating tag for current city
-        const currentCityTemp = document.createElement("p");
-        const currentCityHumidity = document.createElement("p");
-        const currentCityWind = document.createElement("p");
-        const currentCityUVIndex = document.createElement("p");
-        
-        currentCity.textContent = data.name; //grab the name from the data
-        currentCityTemp.textContent = `Temp: ${data.main.temp} °F`;
-        currentCityHumidity.textContent = `Humidity: ${data.main.humidity} %`;
-        currentCityWind.textContent = `Wind: ${data.wind.speed} MPH`;
-        currentCityUVIndex.textContent = `UV Index: `
-
-        currentCityweatherEl.append(currentCity, currentCityTemp, currentCityHumidity, currentCityWind);
-      });
-    }
-  });
+/////     Load search history on page load     /////
+const loadSearchHistory = function () {
+  const loadCityHistory = localStorage.getItem("cityHistory");
+  const parsedCityHistory = JSON.parse(loadCityHistory);
+  console.log(parsedCityHistory);
+  parsedCityHistory.forEach(cityButtonFactory);
 };
 
 /*
@@ -91,8 +106,9 @@ console.log(testQueryURL);
 
 //////////////////////////////////////////////////////////////////////////////////////////
 // add event listeners
-citySearchButton.addEventListener("click", citySearchHandler);
 citySearchButton.addEventListener("click", getCurrentWeatherHandler);
+window.onload = loadSearchHistory;
+
 //////////////////////////////////////////////////////////////////////////////////////////
 /*GIVEN a weather dashboard with form inputs
 WHEN I search for a city
